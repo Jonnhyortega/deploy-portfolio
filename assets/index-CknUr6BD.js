22483e404d591a6c53a0dd35f53b7ec6e6822e62
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2025-10-05 16:59:46 -0300

    Deploy automatico desde script

--- assets/index-CknUr6BD.js
+++ assets/index-CknUr6BD.js
@@ -84,8 +84,7 @@ Error generating stack: `+i.message+`
   text-align: center;
   animation: ${Nn} 0.8s ease forwards;
   min-height: 100vh;
-
-  background: pink;
+  
   div {
     display: flex;
     flex-wrap: wrap;
